cfa75d1317de256e94fe71fc2483784e02a9191d
aryadhira <aryadhira25@gmail.com>
2024-01-18 09:19:17 +0700

    edit without newrelics

--- main.go
+++ main.go
@@ -3,28 +3,30 @@ package main
 import (
 	"log"
 	"net/http"
-
-	"github.com/newrelic/go-agent/v3/newrelic"
+	"time"
 )
 
 func main() {
 
-	app, err := newrelic.NewApplication(
-		newrelic.ConfigAppName("simpleapp"),
-		newrelic.ConfigLicense("3bcca23434ab0d41dd9f6a483f31a88dFFFFNRAL"),
-		newrelic.ConfigAppLogForwardingEnabled(true),
-	)
+	// app, err := newrelic.NewApplication(
+	// 	newrelic.ConfigAppName("simpleapp"),
+	// 	newrelic.ConfigLicense("3bcca23434ab0d41dd9f6a483f31a88dFFFFNRAL"),
+	// 	newrelic.ConfigAppLogForwardingEnabled(true),
+	// )
 	mux := http.NewServeMux()
-	// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	text := "hello world"
-
-	// 	w.Write([]byte(text))
-	// })
-	mux.HandleFunc(newrelic.WrapHandleFunc(app, "/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("API hits on", time.Now())
 		text := "hello world"
 
 		w.Write([]byte(text))
-	}))
+	})
+	// mux.HandleFunc(newrelic.WrapHandleFunc(app, "/", func(w http.ResponseWriter, r *http.Request) {
+	// 	log.Println("API hits on", time.Now())
+
+	// 	text := "hello world"
+
+	// 	w.Write([]byte(text))
+	// }))
 
 	server := new(http.Server)
 	server.Handler = mux
@@ -32,7 +34,7 @@ func main() {
 
 	log.Println("web server is starting at ", server.Addr)
 
-	err = server.ListenAndServe()
+	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatal(err.Error())
 	}
