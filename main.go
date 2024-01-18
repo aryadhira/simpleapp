cd0000af7b1ee354c6cb010f3987d60bb2a30c28
aryadhira <aryadhira25@gmail.com>
2024-01-18 08:28:38 +0700

    add new relic

--- main.go
+++ main.go
@@ -3,15 +3,28 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/newrelic/go-agent/v3/newrelic"
 )
 
 func main() {
+
+	app, err := newrelic.NewApplication(
+		newrelic.ConfigAppName("simpleapp"),
+		newrelic.ConfigLicense("3bcca23434ab0d41dd9f6a483f31a88dFFFFNRAL"),
+		newrelic.ConfigAppLogForwardingEnabled(true),
+	)
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	text := "hello world"
+
+	// 	w.Write([]byte(text))
+	// })
+	mux.HandleFunc(newrelic.WrapHandleFunc(app, "/", func(w http.ResponseWriter, r *http.Request) {
 		text := "hello world"
 
 		w.Write([]byte(text))
-	})
+	}))
 
 	server := new(http.Server)
 	server.Handler = mux
@@ -19,7 +32,7 @@ func main() {
 
 	log.Println("web server is starting at ", server.Addr)
 
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 	if err != nil {
 		log.Fatal(err.Error())
 	}
